c7f1a06a97858b6ea253af45ddd29a009fc1dec3
FilipeGamer12 <74568043+FilipeGamer12@users.noreply.github.com>
2025-03-06 21:27:19 -0300

    atualizção

--- scripts/jogador.gd
+++ scripts/jogador.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
 const SPEED = 200.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -420.0
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
