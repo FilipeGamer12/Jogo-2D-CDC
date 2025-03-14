504ff012cc2677f97a601e893bd6d965a65f1322
FilipeGamer12 <74568043+FilipeGamer12@users.noreply.github.com>
2025-03-13 21:34:30 -0300

    versão atualizada
    paralax, novo player e animações de tiles

--- entidades/playerV2.gd
+++ entidades/playerV2.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 
-const SPEED = 100.0
+const SPEED = 120.0
 const JUMP_VELOCITY = -310.0
 
 func _physics_process(delta: float) -> void:
