3813b581056c1ed04639dc1b0d8fdf5d22673dda
FilipeGamer12 <74568043+FilipeGamer12@users.noreply.github.com>
2025-04-17 20:08:36 -0300

    atualização

--- scripts/nigga.gd
+++ scripts/nigga.gd
@@ -1,18 +1,27 @@
 extends CharacterBody2D
-const SPEED = 120.0
+const SPEED = 50.0
 
 @onready var anim: AnimatedSprite2D = $AnimatedSprite2D
 @onready var hitbox: Area2D = $Hitbox
 @onready var timer: Timer = $Timer
+@onready var det_chão: RayCast2D = $"Detector de chão"
+@onready var det_player: RayCast2D = $"Detector de Player"
+
+var direction = 1 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	velocity.x = SPEED * direction
+	if not det_chão.is_colliding():
+		direction *= -1
+		scale.x *= -1
 	move_and_slide()
 
 func take_damage():
 	print("danou-se")
+	direction = 0
 	anim.play("morto")
 	hitbox.queue_free()
 	timer.start()
