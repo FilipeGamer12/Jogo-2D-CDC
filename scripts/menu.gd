d873125558638ae14f3a1d267a2e316d412bcd9b
Pedro Henrique Serafini <111706609+PedroSerafini@users.noreply.github.com>
2025-05-29 22:56:51 -0300

    criando telas

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -3,3 +3,7 @@ extends Node2D
 
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/fase_1.tscn")
+
+
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/credits.tscn")
